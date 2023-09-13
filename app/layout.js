04595e9b4d2a471d2c21331cd4680fd9c350f78a
Fahim Shakil <fsrafisocial18@gmail.com>
2023-09-13 20:24:15 +0600

    fixing a bug

--- app/layout.js
+++ app/layout.js
@@ -3,10 +3,10 @@ import Copyright from "@/components/Copyright";
 import Navbar from "@/components/Navbar";
 import styles from "@/styles/globals.css";
 import { AuthContextProvider } from "./context/AuthContext";
-export const metadata = {
-  title: "FSR Stories",
-  description: "A place for Reading & Sharing life stories",
-};
+// export const metadata = {
+//   title: "FSR Stories",
+//   description: "A place for Reading & Sharing life stories",
+// };
 
 export default function RootLayout({ children }) {
   return (
